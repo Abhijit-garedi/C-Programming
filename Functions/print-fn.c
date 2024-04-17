745838d58bc206caed91faca8d12080257b6df96
Abhijit-garedi <paljustwin@gmail.com>
2024-04-17 12:27:41 +0530

    print function

---
+++ Functions/print-fn.c
@@ -0,0 +1,12 @@
+// very simple 10 line c program of print function
+
+#include <stdio.h>
+
+void printHello() {
+    printf("Hello, world!\n");
+}
+
+int main() {
+    printHello();
+    return 0;
+}
