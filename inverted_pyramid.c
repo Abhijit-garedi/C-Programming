5abe214a8199d0726b4be46d921e1771fb2d03ed
Abhijit-garedi <paljustwin@gmail.com>
2024-04-21 17:53:32 +0530

    ulta pyramid

---
+++ inverted_pyramid.c
@@ -0,0 +1,20 @@
+// inverted pyramid or ulta pyramid
+
+#include <stdio.h>
+
+int main() {
+    int rows, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i = rows; i >= 1; --i) {
+        for(j = 1; j <= rows-i; ++j)
+            printf("  ");
+        for(j = 0; j != 2*i-1; ++j)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
