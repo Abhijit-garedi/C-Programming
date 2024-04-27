9e4b30756c40f4d7ead767107c35ef2db60b1f98
Abhijit-garedi <paljustwin@gmail.com>
2024-04-27 21:55:52 +0530

    leap year or not

---
+++ If else/leap_year.c
@@ -0,0 +1,25 @@
+// c program to find given year is leap or not
+ 
+
+ #include <stdio.h>
+
+int main() 
+{
+    int year;
+
+    
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) 
+    {
+        printf("%d is a leap year.\n", year);
+    }
+     else 
+    {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
