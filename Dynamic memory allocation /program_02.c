4aa6ee053424737972510b37b5578c3f43c89bbe
Abhijit-garedi <paljustwin@gmail.com>
2024-04-29 20:30:30 +0530

    work left in it

---
+++ Dynamic memory allocation /program_02.c
@@ -0,0 +1,20 @@
+// a simple porgram to find the memory allcoation for the 
+// dynamic program 
+
+
+ #include <stdio.h>
+  #include <stdlib.h>
+     
+      int main()
+       {
+        int *arr;
+        int size, i;
+
+
+        //Ask the user to input the size of the array
+        printf("Enter the size of the array; ");
+        scanf("%d",&size);
+        
+
+
+       }
