26a1e72630dd5adbe1e831c827787f21dc4eac13
Abhijit-garedi <paljustwin@gmail.com>
2024-05-02 00:54:15 +0530

    work left on it

---
+++ If else/cp_sp.c
@@ -0,0 +1,10 @@
+// a simple c program using if else to show whether it is profit or loss
+
+
+ #include <stdio.h>
+    int main()
+     {
+        int cp,sp,a;
+
+        printf("enter the ")
+     }
